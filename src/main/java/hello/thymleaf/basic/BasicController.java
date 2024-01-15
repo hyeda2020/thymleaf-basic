83d3e13385a36c21967dfae6295caecf46e76b4a
hyeda2020 <hyeda2020@gmail.com>
2024-01-15 23:58:26 +0900

    타임리프 기본 객체 / 문자, 숫자, 날짜, URI 유틸리티 / 리터럴 및 연산 기능

--- src/main/java/hello/thymleaf/basic/BasicController.java
+++ src/main/java/hello/thymleaf/basic/BasicController.java
@@ -1,11 +1,16 @@
 package hello.thymleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +32,9 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    /**
+     * 스프링 EL이라는 스프링이 제공하는 변수 표현식 사용
+     */
     @GetMapping("/variable")
     public String textVariable(Model model) {
 
@@ -46,6 +54,63 @@ public class BasicController {
         model.addAttribute("userMap", map);
 
         return "basic/variable";
+    }
+
+    /***
+     * 타임리프에서 제공하는 기본 객체들 사용
+     */
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    /***
+     * 타임리프가 제공하는 날짜 유틸리티 사용
+     */
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    /**
+     * 타임리프가 제공하는 URL링크 유틸리티 사용
+     */
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    /**
+     * 타임리프에서 리터럴 문자 사용
+     */
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    /**
+     * 타임리프 연산 기능 사용
+     */
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
     }
 
     @Data
